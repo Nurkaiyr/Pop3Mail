62688040efe4e339c3de8577d81e812ab3c75bed
Едиге Нуркайыр Маратулы <@corp.itstep>
2018-06-05 11:15:03 +0600

    1

---
+++ MailPOP3/Program.cs
@@ -0,0 +1,38 @@
+﻿using Limilabs.Client.POP3;
+using Limilabs.Mail;
+using Limilabs.Mail.MIME;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailPOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Pop3 pop3 = new Pop3())
+            {              
+                pop3.ConnectSSL("pop.mail.ru", 995);
+                pop3.Login("pop3tester1@mail.ru", "Test12345");
+                foreach (string uid in pop3.GetAll())
+                {
+                    IMail email = new MailBuilder()
+                        .CreateFromEml(pop3.GetMessageByUID(uid));
+                    Console.WriteLine(email.Subject);
+                    Console.WriteLine(email.Text);
+
+                    foreach (MimeData mime in email.NonVisuals)
+            {
+                mime.Save(@"d:\temp\" + mime.SafeFileName);
+            }
+
+                }
+                pop3.Close(false);
+                Console.ReadLine();
+            }
+        }
+    }
+}
